c580d3469ab3a6d0daf55b56dc9033b6c9ae44af
Integral Team <repo@sfxdx.ru>
2020-02-13 15:32:36 +0700

    initial

---
+++ precache-manifest.eee8a5c87aba67f23642cb8d2d2ac458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad1826c6441b12d3ccda728b9d584bd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5593d6e7d8965a9b7d8f",
+    "url": "./static/css/main.b333677f.chunk.css"
+  },
+  {
+    "revision": "f777856797ae1806302a",
+    "url": "./static/js/2.cbb4e3f1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.cbb4e3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5593d6e7d8965a9b7d8f",
+    "url": "./static/js/main.553d0ee1.chunk.js"
+  },
+  {
+    "revision": "30ed7b1ad5985edb78b4",
+    "url": "./static/js/runtime-main.0a632720.js"
+  }
+]);
